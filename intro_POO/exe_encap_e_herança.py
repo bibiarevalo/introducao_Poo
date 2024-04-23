dc3bca571da6c575306fa8844426d467ed82c203
Beatriz Arevalo Freitas <131297673+bibiarevalo@users.noreply.github.com>
2024-04-22 21:41:06 -0300

    Add files via upload

--- intro_POO/exe_encap_e_herança.py
+++ intro_POO/exe_encap_e_herança.py
@@ -7,9 +7,32 @@ class Animal (object):
         print (f'O animal emite som!')
 
 class Cachorro(Animal):
-    def __init__(self, nome, especie, raca):
-        Animal.__init__(nome, especie)
-        self.__raca = 'salsicha'
+    def __init__(self, nome, raca):
+        super().__init__(nome, especie = "Cachoro")
+        self.__raca = raca
 
-    def obter_raca(self, .__raca):
-        print(f'O cachorro é da raça {self.__raca}')    
+    def obter_raca(self):
+        return f'O cachorro é da raça {self.__raca}'
+    
+    def emitir_som(self):
+        return "Au"
+
+class Gato(Animal):
+    def __init__(self, nome, pelagem):
+        super().__init__(nome, "Gato")
+        self.__pelagem = pelagem
+
+    def emitir_som(self):
+        return 'Miau'
+    
+    def obter_pelagem(self):
+        return self.__pelagem
+
+gato = Gato("Luana Carolina Vasconcelos Vegananis", "Parda")
+print(gato.obter_pelagem())
+print(gato.emitir_som())
+
+cachorro = Cachorro("Nina Roberta Carla", "Caramelo")
+print(cachorro.obter_raca())
+print(cachorro.emitir_som())
+        
